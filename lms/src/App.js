0743cc17058854f0bb418edb06dd2a5656c331f3
Shazir-Shafeeque <55185135+Shazir-Shafeeque@users.noreply.github.com>
2019-12-19 16:00:28 +0530

    Navbar

--- lms/src/App.js
+++ lms/src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Catalog from './Components/Catalog'
+import Issue from './Components/Issue'
+import Reservations from './Components/Reservations'
+import Students from './Components/Students'
+import Return from './Components/Return'
+import Navigationbar from './Components/Navigationbar'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Link, BrowserRouter as Router, Switch } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // React Fragments to wrap child elements
+    <> 
+      <Router>
+      <Navigationbar />
+        <Route path='/' exact component = {Catalog} />
+        <Route path='/issue' component = {Issue} />
+        <Route path='/reservations' component = {Reservations} />
+        <Route path='/students' component = {Students} />
+        <Route path='/return' component = {Return} />
+      </Router>
+    </>
   );
 }
 
