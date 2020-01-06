1eb4dcad92feb31ce80d8187c01cc148a1353b27
IreshaSilva96 <57743712+IreshaSilva96@users.noreply.github.com>
2020-01-06 12:19:07 +0530

    Update IssueReservations.js

--- lms/src/Components/IssueReservations.js
+++ lms/src/Components/IssueReservations.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react'; 
 import '../App.css';
 import {Button, Container, Row, Col, Form, Image } from 'react-bootstrap';
-import {Typeahead} from 'react-bootstrap-typeahead';
 
 function IssueReservations(props) {
 
   const [listStudents, setListStudents] = useState([]);
+  const [listReservations, setListReservations] = useState([]);
   const [refresh, setRefresh] = useState(0);
 
   
@@ -24,39 +24,55 @@ function IssueReservations(props) {
       //     )
 
     var searchStudent = listStudents.filter(student => 
-      student.studentFullName.toLowerCase().includes(props.searchString.toString().toLowerCase())
-      )
+      student.fullName.toLowerCase().includes(props.searchString.toString().toLowerCase())
+    )
+      console.log(searchStudent)
+
+    var searchReservations = listReservations.filter(reservation => 
+      reservation.studentFullName.toLowerCase().includes(props.searchString.toString().toLowerCase())
+    )
       console.log(searchStudent)
       
 
   useEffect (() => {
-  fetch("https://localhost:44381/api/reservation")
+      fetch("https://localhost:44381/api/student")
       .then(res => res.json())
       .then(data => {
           setListStudents(data)
+      });
+      fetch("https://localhost:44381/api/reservation")
+      .then(res => res.json())
+      .then(data => {
+          setListReservations(data)
       })    
   },[refresh])
 
     return (
       <>
+      
         <Container>
-        {searchStudent.map(Reservations =>
-        <Col>
+        {searchStudent.length === 1 && 
           <Row>
             <Form.Label className="font-weight-bold" column> Student ID: </Form.Label>
-            <Col className="my-auto">{`S00${Reservations.studentId}`}</Col>
+            <Col className="my-auto">{`S00${searchStudent.studentId}`}</Col>
           </Row>
-          <Row className="mt-3 mb-4 shadow rounded" style={{backgroundColor:"#ededed"}}>
+        }
+        {searchStudent.length === 1 && 
+        <Row>
+            {searchReservations.map(Reservations =>
+            <Row className="mt-3 mb-4 shadow rounded" style={{backgroundColor:"#ededed"}}>
             {/* Image section */}
             <Col md={2}  className="my-auto">
               <Image src={require(`../Coverimages/${Reservations.coverImage === undefined || Reservations.coverImage === null? 'Default.jpg' : Reservations.coverImage}`)} thumbnail style={{width:"100px"}}/>
             </Col>
+           
           </Row>
-        </Col>
-        
-      )}
-        </Container>
+          
 
+          )}
+        </Row>
+       }
+        </Container>
       </>
     );
   }
